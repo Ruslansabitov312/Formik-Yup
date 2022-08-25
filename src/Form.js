41584653b87cc8a74737a5aaf8a129d10f7d18bd
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-08-26 03:28:48 +0600

    Create custom components for Fields(input) === DRY Features

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,35 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field, ErrorMessage, useField } from 'formik'
 import * as Yup from 'yup'
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <>
+      <label htmlFor={props.name}>{label}</label>
+      <input {...props} {...field} />
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
+
+const MyCheckbox = ({ children, ...props }) => {
+  const [field, meta] = useField({ ...props, type: 'checkbox' })
+
+  return (
+    <>
+      <label className='checkbox'>
+        <input type='checkbox' {...props} {...field} />
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
 
 const CustomForm = () => {
   return (
@@ -32,31 +62,32 @@ const CustomForm = () => {
     >
       <Form className='form'>
         <h2>Отправить пожертвование</h2>
-        <label htmlFor='name'>Ваше имя</label>
-        <Field
-          id='name' 
-          name='name' 
+        <MyTextInput
+          label='Ваше имя'
+          id='name'
+          name='name'
           type='text'
         />
-        <ErrorMessage className='error' name='name' component='div' />
-        <label htmlFor='email'>Ваша почта</label>
-        <Field 
-          id='email' 
-          name='email' 
+
+        <MyTextInput
+          label='Ваша почта'
+          id='email'
+          name='email'
           type='email'
         />
-        <ErrorMessage className='error' name='email' component='div' />
+
         <label htmlFor='amount'>Количество</label>
-        <Field 
-          id='amount' 
-          name='amount' 
+        <Field
+          id='amount'
+          name='amount'
           type='number'
         />
         <ErrorMessage className='error' name='amount' component='div' />
+
         <label htmlFor='currency'>Валюта</label>
-        <Field 
-          id='currency' 
-          name='currency' 
+        <Field
+          id='currency'
+          name='currency'
           as='select'
         >
           <option value=''>Выберите валюту</option>
@@ -65,21 +96,18 @@ const CustomForm = () => {
           <option value='RUB'>RUB</option>
         </Field>
         <ErrorMessage className='error' name='currency' component='div' />
+
         <label htmlFor='text'>Ваше сообщение</label>
-        <Field 
-          id='text' 
-          name='text' 
+        <Field
+          id='text'
+          name='text'
           as='textarea'
         />
         <ErrorMessage className='error' name='text' component='div' />
-        <label className='checkbox'>
-          <Field 
-            name='terms' 
-            type='checkbox'
-          />
+        
+        <MyCheckbox name='terms'>
           Соглашаетесь с политикой конфиденциальности?
-        </label>
-        <ErrorMessage className='error' name='terms' component='div' />
+        </MyCheckbox>
         <button type='submit'>Отправить</button>
       </Form>
     </Formik>
